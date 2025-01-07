00b553e418fa8c737fdc6e40201893eea92a1929
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-07 20:31:11 +0200

    Part01

---
+++ Assig03/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Assig03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region What is the primary purpose of an interface in C#?
+            //b) To define a blueprint for a class
+            #endregion
+
+            #region Which of the following is NOT a valid access modifier for interface members in C#?
+            //a) private
+            #endregion
+
+            #region Can an interface contain fields in C#?
+            // b) No
+            #endregion
+
+            #region In C#, can an interface inherit from another interface?
+            //b) Yes, interfaces can inherit from multiple interfaces
+            #endregion
+
+            #region Which keyword is used to implement an interface in a class in C#?
+            // d) implements
+            #endregion
+
+            #region Can an interface contain static methods in C#?
+            //a) Yes
+            #endregion
+
+            #region In C#, can an interface have explicit access modifiers for its members?
+            // b) No, all members are implicitly public
+            #endregion
+
+            #region What is the purpose of an explicit interface implementation in C#? 
+            // b) To provide a clear separation between interface and class members
+            #endregion
+
+            #region In C#, can an interface have a constructor?
+            //b) No, interfaces cannot have constructors
+            #endregion
+
+            #region How can a C# class implement multiple interfaces?
+            //c) By separating interface names with commas
+            #endregion
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
