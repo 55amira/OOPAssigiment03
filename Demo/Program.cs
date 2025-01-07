98e82c9709b6d7b2a26863de527020245eee5f6a
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-07 14:13:27 +0200

    interfaceEx01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Demo
+﻿using Demo.interfaceEx01;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            #region interfaceEx01
+            //MyType myType= new MyType();
+            //myType.Fun();
+            //myType.salary = 12000;
+            //Console.WriteLine(myType.salary);
+
+            //IMyType myType;
+            //myType = new MyType();
+            //myType.Print();
+            //myType.Fun();
+            //myType.salary = 12000;
+            //Console.WriteLine(myType.salary); 
+            #endregion
+
         }
     }
 }
---
+++ Demo/interfaceEx01/IMyType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.interfaceEx01
+{
+    internal interface IMyType
+    {
+
+        //Signature Of Methods
+        void Fun();
+
+        //Signature Of Properties
+       double salary { get; set; }
+
+        //Default Implmented Methods
+
+        void Print()
+        {
+            Console.WriteLine("Default Implmented Methods");
+        }
+    }
+}
